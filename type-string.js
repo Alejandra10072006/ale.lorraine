441fd445c7a33b6097b3e6c30c3b46dc84b0e2f2
Alejandra10072006 <145460785+Alejandra10072006@users.noreply.github.com>
2024-03-05 17:04:22 -0300

    Add files via upload

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mumdo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao =  "Meu nome é ";
+const meuNome = "Alejandra";
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string ou template literal 
